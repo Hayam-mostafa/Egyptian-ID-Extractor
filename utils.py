1c0d775dfdb46151045284754dba69a091e066ef
Hayam Mostafa <142169839+Hayam-mostafa@users.noreply.github.com>
2026-07-06 03:26:16 +0300

    Integrate CRNN model and enhance image processing
    Refactored utils.py to integrate CRNN model for national ID detection and improved image processing functions.

--- utils.py
+++ utils.py
@@ -1,19 +1,97 @@
-from ultralytics import YOLO
-import easyocr
 import cv2
 import numpy as np
-import re
+import calendar
+import torch
+import torch.nn as nn
+from ultralytics import YOLO
+from info import parse_egyptian_id, GOVERNORATES
+
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 card_model   = YOLO('Models/card_detector.pt')
 nid_model     = YOLO('Models/nid_detector.pt')
-digit_model = YOLO('Models/digit_detector.pt')
-reader       = easyocr.Reader(['ar'], gpu=False)
-clahe       = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(8,8)) 
+
+# CRNN Model
+CHARS = "0123456789"
+BLANK_IDX = 10
+
+idx_to_char = {i: c for i, c in enumerate(CHARS)}
+
+class BidirectionalLSTM(nn.Module):
+    def __init__(self, in_size, hidden, out_size):
+        super().__init__()
+        self.rnn = nn.LSTM(in_size, hidden, bidirectional=True, batch_first=True)
+        self.fc = nn.Linear(hidden * 2, out_size)
+
+    def forward(self, x):
+        out, _ = self.rnn(x)
+        return self.fc(out)
+
+
+class CRNN(nn.Module):
+    def __init__(self, num_classes, rnn_hidden=256, rnn_layers=2, dropout=0.3):
+        super().__init__()
+
+        def conv_bn_relu(ci, co):
+            return nn.Sequential(
+                nn.Conv2d(ci, co, 3, 1, 1, bias=False),
+                nn.BatchNorm2d(co),
+                nn.ReLU(inplace=True),
+            )
+
+        self.cnn = nn.Sequential(
+            conv_bn_relu(1, 64),
+            nn.MaxPool2d(2, 2),
+
+            conv_bn_relu(64, 128),
+            nn.MaxPool2d(2, 2),
+
+            conv_bn_relu(128, 256),
+            conv_bn_relu(256, 256),
+            nn.MaxPool2d((2,1),(2,1)),
+
+            conv_bn_relu(256, 512),
+            conv_bn_relu(512, 512),
+            nn.MaxPool2d((2,1),(2,1)),
+
+            conv_bn_relu(512, 512),
+            nn.Dropout2d(dropout),
+        )
+
+        self.pool = nn.AdaptiveAvgPool2d((1, None))
+
+        layers = []
+        for i in range(rnn_layers):
+            in_size = 512 if i == 0 else rnn_hidden * 2
+            out_size = num_classes if i == rnn_layers - 1 else rnn_hidden * 2
+            layers.append(BidirectionalLSTM(in_size, rnn_hidden, out_size))
+
+        self.rnn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.cnn(x)
+        x = self.pool(x)
+        x = x.squeeze(2).permute(0, 2, 1)
+        x = self.rnn(x)
+        return x.permute(1, 0, 2)
+
+
+crnn_model = CRNN(
+    num_classes=len(CHARS) + 1,
+    rnn_hidden=256,
+    rnn_layers=2,
+    dropout=0.3
+).to(DEVICE)
+
+checkpoint = torch.load("Models/best_crnn.pth", map_location=DEVICE)
+crnn_model.load_state_dict(checkpoint["model"])
+crnn_model.eval()
 
 
 #detect and crop the card from the input image
 def crop_card(image):
-    prediction_results = card_model.predict(image, conf=0.5, iou=0.45)
+    prediction_results = card_model.predict(image, conf=0.5, iou=0.45 , verbose=False)
     prediction_result  = prediction_results[0]
 
     if len(prediction_result.boxes) == 0:
@@ -44,75 +122,110 @@ def crop_card(image):
 
 #correct the orientation of the cropped card
 def correct_orientation(image_cropped):
+
+    small = cv2.resize(image_cropped, (128, 128))
+
+    rotations = [
+        small,
+        cv2.rotate(small, cv2.ROTATE_90_CLOCKWISE),
+        cv2.rotate(small, cv2.ROTATE_180),
+        cv2.rotate(small, cv2.ROTATE_90_COUNTERCLOCKWISE),
+    ]
+
+    results = nid_model.predict(
+        rotations,
+        conf=0.4,
+        verbose=False
+    )
+
     angles = [0, 90, 180, 270]
+
     best_angle = 0
-    max_conf = 0
+    best_score = 0
 
-    h, w = image_cropped.shape[:2]
-    small = cv2.resize(image_cropped, (320, 320))
+    for i, result in enumerate(results):
 
-    for angle in angles:
-        if angle == 90:
-            rotated = cv2.rotate(small, cv2.ROTATE_90_CLOCKWISE)
-        elif angle == 180:
-            rotated = cv2.rotate(small, cv2.ROTATE_180)
-        elif angle == 270:
-            rotated = cv2.rotate(small, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        else:
-            rotated = small
+        if len(result.boxes) > 0:
 
-        results = nid_model.predict(rotated, conf=0.4, verbose=False)[0]
+            confs = result.boxes.conf.cpu().numpy()
 
-        if len(results.boxes) > 0:
-            current_conf = results.boxes.conf.cpu().numpy().max()
+            score = confs.sum()
 
-            if current_conf > max_conf:
-                max_conf = current_conf
-                best_angle = angle
+            if score > best_score:
+                best_score = score
+                best_angle = angles[i]
 
     if best_angle == 90:
         return cv2.rotate(image_cropped, cv2.ROTATE_90_CLOCKWISE)
+
     elif best_angle == 180:
         return cv2.rotate(image_cropped, cv2.ROTATE_180)
+
     elif best_angle == 270:
         return cv2.rotate(image_cropped, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
     return image_cropped
 
 
+#correct the skew of the orientation corrected image
 def correct_skew(image):
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.GaussianBlur(gray, (3,3), 0)
+
     edges = cv2.Canny(gray, 50, 150)
 
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=120)
+    lines = cv2.HoughLinesP(
+        edges,
+        1,
+        np.pi / 180,
+        threshold=80,
+        minLineLength=100,
+        maxLineGap=10
+    )
 
     if lines is None:
         return image
 
     angles = []
+
     for line in lines:
-        rho, theta = line[0]
-        angle = (theta * 180 / np.pi) - 90
+
+        x1, y1, x2, y2 = line[0]
+
+        angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
 
         if -20 < angle < 20:
             angles.append(angle)
 
-    if len(angles) < 5:  
+    if len(angles) == 0:
         return image
 
     median_angle = np.median(angles)
 
-    if abs(median_angle) < 2 or abs(median_angle) > 20:
-        return image  
+    if abs(median_angle) < 1:
+        return image
 
-    (h, w) = image.shape[:2]
-    M = cv2.getRotationMatrix2D((w//2, h//2), median_angle, 1.0)
+    h, w = image.shape[:2]
 
-    rotated = cv2.warpAffine(image, M, (w, h),flags=cv2.INTER_CUBIC,borderMode=cv2.BORDER_REPLICATE)
+    M = cv2.getRotationMatrix2D(
+        (w // 2, h // 2),
+        median_angle,
+        1.0
+    )
+
+    rotated = cv2.warpAffine(
+        image,
+        M,
+        (w, h),
+        flags=cv2.INTER_CUBIC,
+        borderMode=cv2.BORDER_REPLICATE
+    )
 
     return rotated
-    
-    
+
+
 #detect and crop the ID region from the corrected image
 def crop_id_box(image, pad=5):
     result = nid_model.predict(image, conf=0.5, verbose=False)[0]
@@ -134,93 +247,87 @@ def crop_id_box(image, pad=5):
     return id_cropped
 
 
-#Function to detect the national ID number using YOLO
-def detect_national_id(id_cropped):
-
-    results = digit_model.predict(id_cropped, conf=0.3, iou=0.5, verbose=False)[0]
-
-    if len(results.boxes) == 0:
-        return ""
-
-    boxes     = results.boxes.xyxy.cpu().numpy()
-    class_ids = results.boxes.cls.cpu().numpy()
-    confs     = results.boxes.conf.cpu().numpy()
-
-    if len(class_ids) > 14:
-        num14    = np.argsort(confs)[-14:]
-        boxes    = boxes[num14]
-        class_ids = class_ids[num14]
-
-    sorted_idx = np.argsort(boxes[:, 0])
-    return ''.join([str(int(class_ids[i])) for i in sorted_idx]) 
-
-
 #Function to preprocess the cropped image for OCR
 def preprocess_image(image):
-    gray     = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_up  = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
-    denoised = cv2.fastNlMeansDenoising(gray_up, None, h=7)
-    enhanced = clahe.apply(denoised)
-    binary   = cv2.adaptiveThreshold(
-        enhanced, 255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY, 31, 9
-    )
-    kernel = np.ones((2, 2), np.uint8)
-    binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
-    return binary
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, (256, 64))
+
+    norm = resized.astype(np.float32) / 255.0
+    norm = (norm - 0.5) / 0.5
+
+    tensor = torch.tensor(norm).unsqueeze(0).unsqueeze(0)
+    return tensor.to(DEVICE)
 
 
-#Function to convert Arabic digits to English digits
-def convert_arabic_to_english(text):
-    return text.translate(str.maketrans("٠١٢٣٤٥٦٧٨٩", "0123456789"))
+#Function to detect the national ID number using YOLO
+def detect_national_id(id_cropped):
+    img = preprocess_image(id_cropped)
+
+    with torch.no_grad():
+        output = crnn_model(img)
+
+    return decode_crnn(output)
 
 
-#Function to extract national ID number using OCR
-def extract_national_id(id_cropped):
-    binary = preprocess_image(id_cropped)
-    
-    result = reader.readtext(
-        binary,
-        allowlist='0123456789٠١٢٣٤٥٦٧٨٩',
-        detail=1,
-        paragraph=False,
-        decoder='beamsearch',
-        beamWidth=10,
-        contrast_ths=0.1,
-        adjust_contrast=0.5,
-        text_threshold=0.2,
-        low_text=0.2
-    )
-    
-    ocr_digits = ""
-    for box, text, _ in sorted(result, key=lambda x: x[0][0][0]):
-        ocr_digits += re.sub(r'\D', '', convert_arabic_to_english(text))
+#Decoding the CRNN output to  the predicted ID number
+def decode_crnn(output):
 
-    yolo_digits = detect_national_id(id_cropped)
-   
-    if len(yolo_digits) == 14:
-        return yolo_digits
+    output = output.permute(1, 0, 2)
+    preds = output.argmax(2)
 
-    elif len(ocr_digits) == 14:
-        return ocr_digits
+    pred = preds[0]
 
-    else:
-        return "Unverified"
+    chars = []
+    prev = None
+
+    for p in pred:
+
+        p = p.item()
+
+        if p != BLANK_IDX and p != prev:
+            chars.append(idx_to_char[p])
+
+        prev = p
+
+    return "".join(chars)
+
+
+
+def validate_egyptian_id(nid):
+    parsed = parse_egyptian_id(nid)
+    if parsed is None:
+        return False
+
+    if not (1 <= parsed['month'] <= 12):
+        return False
+
+    try:
+        max_day = calendar.monthrange(parsed['full_year'], parsed['month'])[1]
+    except ValueError:
+        return False
+
+    if not (1 <= parsed['day'] <= max_day):
+        return False
+
+    if parsed['governorate_code'] not in GOVERNORATES:
+        return False
+
+    return True
+
 
 '''full pipeline from image to national ID number'''
-def extract_nid_from_image(input_img):
+def extract_national_id(input_img):
 
     img_array = np.frombuffer(input_img, np.uint8)
     image     = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
     if image is None:                       
-        return None, None, "Invalid Image"
+        raise ValueError("Invalid Image")
  
     image_cropped = crop_card(image)
     
     if image_cropped is None:
-        return image, None, "Card Not Detected"
+       raise ValueError("Card Not Detected")
 
     corrected_orientation = correct_orientation(image_cropped)
 
@@ -229,9 +336,11 @@ def extract_nid_from_image(input_img):
     id_cropped = crop_id_box(corrected_skew)
 
     if id_cropped is None:
-      return corrected_skew , None, "ID Not Detected"
-    
-    nid = extract_national_id(id_cropped)
+       raise ValueError("ID Not Detected")
+
+    nid = detect_national_id(id_cropped)
+
+    if not validate_egyptian_id(nid):
+        raise ValueError("Please Retake a clearer image")
 
     return corrected_skew, id_cropped, nid
-
